edb0701e1d691049c2e561586d526b92eeac347b
vividvinyliticsstickers <vividvinylicsstickers@gmail.com>
2026-06-01 19:13:57 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
-  },
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: '**' }],
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   output: 'standalone',
 }
